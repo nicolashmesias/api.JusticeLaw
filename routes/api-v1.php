6fba60ed011b6e4f93d4d73f5b67d0219df48400
majosetax <mariajolopezsc@gmail.com>
2024-11-25 14:59:05 -0600

    prueba login roles

--- routes/api-v1.php
+++ routes/api-v1.php
@@ -59,13 +59,15 @@ Route::group([
 ], function ($router) {
     Route::post('/register', [AuthController::class, 'register'])->name('register');
     // Route::post('/login', [AuthController::class, 'login'])->name('login');
-    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
     Route::post('/refresh', [AuthController::class, 'refresh'])->name('refresh');
     Route::post('/me', [AuthController::class, 'me'])->name('me');
 
 });
 
-Route::post('/loginActualizacion', [AuthController::class, 'login'])->name('login');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+
+Route::post('/login', [AuthController::class, 'login'])->name('login');
 
 Route::post('/registerLawyer', [AuthController::class, 'registerLawyer'])->name('registerPRUEBA')->middleware('guest');
 
